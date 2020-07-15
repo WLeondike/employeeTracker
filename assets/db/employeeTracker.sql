89728d88e8eab882fb96993dc9ae8377687792ff
WLeondike <billyleondike@gmail.com>
2020-07-14 22:01:45 -0400

    added seeds.sql, updates employeeTracker sql

--- assets/db/employeeTracker.sql
+++ assets/db/employeeTracker.sql
@@ -4,24 +4,28 @@ CREATE database employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE employee (
-  id int(10) AUTO_INCREMENT NOT NULL,
-  first_name varchar(30) NOT NULL,
-  last_name varchar(30) NOT NULL,
-  role_id int NOT NULL,
-  manager_id int NOT NULL,
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE empRole(
-  id int(10) AUTO_INCREMENT NOT NULL,
-  title varchar(30) NOT NULL,
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
   salary decimal NOT NULL,
-  department_id int NOT NULL,
+  department_id INT NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE empDepartment(
-  id int(10) AUTO_INCREMENT NOT NULL,
-  name varchar(30) NOT NULL,
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id) 
 );
+
+SELECT * FROM employee;
+SELECT * FROM empRole;
+SELECT * FROM empDepartment;
---
+++ assets/db/seeds.sql
@@ -0,0 +1,17 @@
+USE employee_trackerDB
+
+-- template --
+-- INSERT INTO ()
+-- VALUES ("");
+
+INSERT INTO employee (first_name)
+VALUES ("Jake"), ("Kevin"), ("Suzan"), 
+       ("Maria"), ("Chris"), ("David"), 
+       ("Jose"), ("Nikki"), ("George"),
+       ("Juan"), ("Haylee"), ("Jane");
+
+INSERT INTO employee (last_name)
+VALUES ("Smith"), ("Jones"), ("Brown"),
+       ("Williams"), ("Johnson"), ("Lee"),
+       ("Rodriguez"), ("Garcia"), ("Gonzalez"),
+       ("Lopez"), ("Jones"), ("Pearson");
