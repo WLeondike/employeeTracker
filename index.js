75145bdc9b85373b31574017fab7e316be6f1b5a
WLeondike <billyleondike@gmail.com>
2020-07-14 21:40:43 -0400

    added port, updated json info

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
   host: "localhost",
+  port: "3306",
   user: "root",
   password: "WLeondike",
   database: "employee_trackerDB",
