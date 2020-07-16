9515e284dfe00758d9bc1529153045c5613c99e0
WLeondike <billyleondike@gmail.com>
2020-07-16 19:06:40 -0400

    add functions working

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const connection = mysql.createConnection({
   database: "employee_trackerDB",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-  // options();
+  options();
 });
 
 // connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
@@ -38,7 +38,7 @@ function options() {
         //"Remove Role", //Bonus
         "Update Employee Role",
         //"Update Employee Manager", //Bonus
-        "Exit"
+        "Exit",
       ],
     })
     .then(function (answer) {
@@ -77,18 +77,124 @@ function options() {
       }
     });
 }
-function viewEmp() {};
 
-function viewRole() {};
+function viewEmp() {
+  // return this.connection.query(
+  //   `SELECT employee.emp_id, employee.first_name, employee.last_name, empRole.title, empDepartment.dep_name
+  //   AS empDepartment, empRole.salary, CONCAT(manager.first_name, '', manager.last_name)
+  //   AS manager FROM employee
+  //   LEFT JOIN empRole ON employee.role_id = empRole.role_id
+  //   LEFT JOIN empDepartment ON empRole.department_id = empDepartment.dep_id
+  //   LEFT JOIN employee a ON a.employee_id = employee.manager_id`
+  // );
+}
 
-function viewDep() {};
+function viewRole() {}
 
-function addEmp() {};
+function viewDep() {}
 
-function addDep() {};
+function addEmp() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "First name?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Last name?",
+      },
+      {
+        type: "input",
+        name: "roleID",
+        message: "Employee's role ID?",
+      },
+      {
+        type: "input",
+        name: "managerID",
+        message: "What is the manager ID number?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: answer.firstName,
+          last_name: answer.lastName,
+          role_id: answer.roleID,
+          manager_id: answer.managerID
+        },
+        function (err) {
+          if (err) throw err;
+          console.log("Employee added! returning...");
+          options();
+        }
+      );
+    });
+}
 
-function addRole() {};
+function addDep() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "department",
+      message: "Name of the new department?",
+    }
+  ])
+  .then(function (answer) {
+    connection.query(
+      "INSERT INTO empDepartment SET ?",
+      {
+        dep_name: answer.department,
+      },
+      function (err) {
+        if (err) throw err;
+        console.log("Department added! returning...");
+        options();
+      }
+    );
+  });
+}
 
-function updateRole() {};
+function addRole() {
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Name of the new role?",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "What is the new role's salary?",
+    },
+    {
+      type: "input",
+      name: "depID",
+      message: "What is the Department ID for the new role?",
+    }
+  ])
+  .then(function (answer) {
+    connection.query(
+      "INSERT INTO empRole SET ?",
+      {
+        title: answer.title,
+        salary: answer.salary,
+        department_id: answer.depID
+      },
+      function (err) {
+        if (err) throw err;
+        console.log("Role added! returning...");
+        options();
+      }
+    );
+  });
+}
 
-options();
+function updateRole() {}
+
+// options();
