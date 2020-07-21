a8876f23126303b7e25d8594cc079e25e60c340a
WLeondike <billyleondike@gmail.com>
2020-07-21 19:24:35 -0400

    made changes to schema to resolve conflits

--- db/schema.sql
+++ db/schema.sql
@@ -8,24 +8,30 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT,
+  manager_id INT
 );
 
 CREATE TABLE empRole(
   role_id INT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
+  department_id INT NOT NULL
 );
 
 CREATE TABLE empDepartment(
   dep_id INT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  dep_name VARCHAR(30) NOT NULL, 
+  dep_name VARCHAR(30) NOT NULL
 );
 
-ALTER TABLE employee
-ADD FOREIGN KEY(role_id) REFERENCES empRole(role_id) ON DELETE CASCADE,
-ADD FOREIGN KEY(manager_id) REFERENCES employee(emp_id) ON DELETE CASCADE
+-- These statements are to be run the after the seeds.sql file has been run 
+-- in order to avoid a conflict with the FK
+-- ------------------------------------------------------------------------
 
-ALTER TABLE empRole
-ADD FOREIGN KEY(department_id) REFERENCES empDepartment(dep_id) ON DELETE CASCADE
+-- ALTER TABLE employee
+-- ADD FOREIGN KEY(role_id) REFERENCES empRole(role_id) ON DELETE CASCADE,
+
+-- ALTER TABLE employee
+-- ADD FOREIGN KEY(manager_id) REFERENCES employee(emp_id) ON DELETE CASCADE
+
+-- ALTER TABLE empRole
+-- ADD FOREIGN KEY(department_id) REFERENCES empDepartment(dep_id) ON DELETE CASCADE
